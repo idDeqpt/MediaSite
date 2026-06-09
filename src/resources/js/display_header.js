51ba41223bf9ffd3464adffb11be8b538a5e1ffd
idDeqpt <ushakov_vl@list.ru>
2026-06-09 22:19:57 +0700

    feat: modify style architecture

--- src/resources/js/display_header.js
+++ src/resources/js/display_header.js
@@ -1,3 +1,3 @@
 function display_header() {
-	document.getElementById("header").innerHTML = "<a onclick=\"window.location.assign('/catalog');\">Название сайта</a>\n";
+	document.getElementById("header").innerHTML = "<a onclick=\"window.location.assign('/catalog');\">MediaSite</a>\n";
 }
